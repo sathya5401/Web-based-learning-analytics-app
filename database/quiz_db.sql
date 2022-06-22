41c8d491927f58b1e835243059966c3cb191dec3
TanKahEik <103479195+TanKahEik@users.noreply.github.com>
2022-06-23 04:40:08 +0800

    database

--- database/quiz_db.sql
+++ database/quiz_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2020 at 03:36 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Jun 22, 2022 at 10:37 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,8 +42,12 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`id`, `user_id`, `quiz_id`, `question_id`, `option_id`, `is_right`, `date_updated`) VALUES
-(5, 12, 2, 4, 32, 1, '2020-09-07 16:59:14'),
-(6, 12, 2, 5, 38, 1, '2020-09-07 16:59:14');
+(7, 16, 5, 9, 54, 0, '2022-06-23 03:53:32'),
+(8, 16, 5, 11, 64, 0, '2022-06-23 03:53:32'),
+(9, 16, 5, 12, 65, 0, '2022-06-23 03:53:32'),
+(10, 17, 5, 9, 82, 1, '2022-06-23 03:55:08'),
+(11, 17, 5, 11, 76, 1, '2022-06-23 03:55:08'),
+(12, 17, 5, 12, 78, 0, '2022-06-23 03:55:08');
 
 -- --------------------------------------------------------
 
@@ -63,7 +67,7 @@ CREATE TABLE `faculty` (
 --
 
 INSERT INTO `faculty` (`id`, `user_id`, `subject`, `date_updated`) VALUES
-(2, 6, 'Math', '2020-09-07 12:02:08');
+(2, 6, 'Math', '2022-06-23 12:02:08');
 
 -- --------------------------------------------------------
 
@@ -85,7 +89,8 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`id`, `quiz_id`, `user_id`, `score`, `total_score`, `date_updated`) VALUES
-(3, 2, 12, 4, 4, '2020-09-07 16:59:14');
+(4, 5, 16, 0, 3, '2022-06-23 03:53:32'),
+(5, 5, 17, 2, 3, '2022-06-23 03:55:08');
 
 -- --------------------------------------------------------
 
@@ -106,8 +111,10 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`id`, `question`, `qid`, `order_by`, `date_updated`) VALUES
-(4, 'asdasd ads ads f ddfg dfgg', 2, 0, '2020-09-07 14:32:18'),
-(5, 'Sample Question', 2, 0, '2020-09-07 14:00:39');
+(9, '1 + 1 =', 5, 0, '2022-06-23 03:51:58'),
+(11, '2 + 2 =', 5, 0, '2022-06-23 03:52:31'),
+(12, '1 * 3 =', 5, 0, '2022-06-23 03:52:51'),
+(13, '123 + 123', 6, 0, '2022-06-23 04:19:15');
 
 -- --------------------------------------------------------
 
@@ -128,14 +135,42 @@ CREATE TABLE `question_opt` (
 --
 
 INSERT INTO `question_opt` (`id`, `option_txt`, `question_id`, `is_right`, `date_updated`) VALUES
-(29, 'dsfsf sdf', 4, 0, '2020-09-07 14:40:57'),
-(30, 'dfdf', 4, 0, '2020-09-07 14:40:57'),
-(31, ' dfd', 4, 0, '2020-09-07 14:40:57'),
-(32, 'dsfsd', 4, 1, '2020-09-07 14:40:57'),
-(37, 'Wrong', 5, 0, '2020-09-07 14:41:32'),
-(38, 'Right', 5, 1, '2020-09-07 14:41:32'),
-(39, 'Wrong', 5, 0, '2020-09-07 14:41:32'),
-(40, 'Wrong', 5, 0, '2020-09-07 14:41:32');
+(45, '101', 7, 0, '2022-06-23 03:31:53'),
+(46, '100', 7, 0, '2022-06-23 03:31:53'),
+(47, '102', 7, 0, '2022-06-23 03:31:53'),
+(48, '105', 7, 0, '2022-06-23 03:31:53'),
+(49, '1', 8, 0, '2022-06-23 03:36:37'),
+(50, '2', 8, 0, '2022-06-23 03:36:37'),
+(51, '3', 8, 0, '2022-06-23 03:36:37'),
+(52, '4', 8, 0, '2022-06-23 03:36:37'),
+(57, '1', 10, 0, '2022-06-23 03:52:01'),
+(58, '2', 10, 0, '2022-06-23 03:52:01'),
+(59, '3', 10, 0, '2022-06-23 03:52:01'),
+(60, '4', 10, 0, '2022-06-23 03:52:02'),
+(73, '1', 11, 0, '2022-06-23 03:54:37'),
+(74, '2', 11, 0, '2022-06-23 03:54:37'),
+(75, '3', 11, 0, '2022-06-23 03:54:37'),
+(76, '4', 11, 1, '2022-06-23 03:54:37'),
+(77, '1', 12, 0, '2022-06-23 03:54:46'),
+(78, '2', 12, 0, '2022-06-23 03:54:46'),
+(79, '3', 12, 1, '2022-06-23 03:54:46'),
+(80, '4', 12, 0, '2022-06-23 03:54:46'),
+(81, '1', 9, 0, '2022-06-23 03:54:50'),
+(82, '2', 9, 1, '2022-06-23 03:54:50'),
+(83, '3', 9, 0, '2022-06-23 03:54:50'),
+(84, '4', 9, 0, '2022-06-23 03:54:50'),
+(85, '123', 13, 0, '2022-06-23 04:19:15'),
+(86, '246', 13, 0, '2022-06-23 04:19:15'),
+(87, '369', 13, 0, '2022-06-23 04:19:15'),
+(88, '111', 13, 0, '2022-06-23 04:19:15'),
+(89, '123', 14, 0, '2022-06-23 04:19:35'),
+(90, '246', 14, 0, '2022-06-23 04:19:35'),
+(91, '369', 14, 0, '2022-06-23 04:19:35'),
+(92, '121', 14, 0, '2022-06-23 04:19:35'),
+(93, '123', 15, 0, '2022-06-23 04:19:35'),
+(94, '246', 15, 0, '2022-06-23 04:19:36'),
+(95, '369', 15, 0, '2022-06-23 04:19:36'),
+(96, '121', 15, 0, '2022-06-23 04:19:36');
 
 -- --------------------------------------------------------
 
@@ -156,7 +191,8 @@ CREATE TABLE `quiz_list` (
 --
 
 INSERT INTO `quiz_list` (`id`, `title`, `qpoints`, `user_id`, `date_updated`) VALUES
-(2, 'Pre-Test (Math)', 2, 6, '2020-09-07 13:21:27');
+(5, 'Quiz (Math)', 1, 6, '2022-06-23 03:56:40'),
+(6, 'Quiz 2 ( Math )', 2, 6, '2022-06-23 04:18:17');
 
 -- --------------------------------------------------------
 
@@ -176,8 +212,13 @@ CREATE TABLE `quiz_student_list` (
 --
 
 INSERT INTO `quiz_student_list` (`id`, `quiz_id`, `user_id`, `date_updated`) VALUES
-(5, 2, 12, '2020-09-07 15:05:58'),
-(6, 2, 13, '2020-09-07 15:05:58');
+(7, 2, 12, '2022-06-23 03:37:41'),
+(8, 5, 16, '2022-06-23 03:52:16'),
+(9, 5, 17, '2022-06-23 03:52:16'),
+(10, 5, 18, '2022-06-23 03:52:16'),
+(11, 5, 19, '2022-06-23 03:52:16'),
+(12, 6, 16, '2022-06-23 04:19:48'),
+(13, 6, 17, '2022-06-23 04:19:48');
 
 -- --------------------------------------------------------
 
@@ -197,8 +238,10 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `user_id`, `level_section`, `date_updated`) VALUES
-(3, 12, '2-C', '2020-09-07 14:51:25'),
-(4, 13, '2-C', '2020-09-07 14:54:28');
+(5, 16, 'Information System', '2022-06-23 03:48:20'),
+(6, 17, 'Information System', '2022-06-23 03:49:51'),
+(7, 18, 'Multimedia', '2022-06-23 03:50:33'),
+(8, 19, 'Multimedia', '2022-06-23 03:50:52');
 
 -- --------------------------------------------------------
 
@@ -221,10 +264,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `user_type`, `username`, `password`, `status`, `date_updated`) VALUES
-(1, 'Administrator', 1, 'admin', 'admin123', 1, '2020-09-07 09:10:49'),
-(6, 'John Smith', 2, 'jsmith', 'admin123', 1, '2020-09-07 10:23:30'),
-(12, 'Sample Student', 3, 'sstudent', 'admin123', 1, '2020-09-07 14:51:25'),
-(13, 'Claire Blake', 3, 'cblake', 'admin123', 1, '2020-09-07 14:54:28');
+(1, 'Administrator', 1, 'admin', 'admin123', 1, '2022-06-23 09:10:49'),
+(6, 'John Smith', 2, 'jsmith', 'admin123', 1, '2022-06-23 10:23:30'),
+(16, 'Zi Xiang', 3, 'zixiang', 'admin123', 1, '2022-06-23 03:48:19'),
+(17, 'Jovi', 3, 'jovi', 'admin123', 1, '2022-06-23 03:49:51'),
+(18, 'Zhen Heng', 3, 'zhenheng', 'admin123', 1, '2022-06-23 03:50:33'),
+(19, 'Kah Eik', 3, 'kaheik', 'admin123', 1, '2022-06-23 03:50:52');
 
 --
 -- Indexes for dumped tables
@@ -292,7 +337,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `faculty`
@@ -304,43 +349,43 @@ ALTER TABLE `faculty`
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `question_opt`
 --
 ALTER TABLE `question_opt`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT for table `quiz_list`
 --
 ALTER TABLE `quiz_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `quiz_student_list`
 --
 ALTER TABLE `quiz_student_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
